84b4a0a5d5f92107c54087de6affb3f4c114da66
MilanaT21 <tasmurzinam@yandex.ru>
2026-08-31 22:24:32 +0300

    Update models.py

--- models.py
+++ models.py
@@ -3,16 +3,28 @@ from flask_login import UserMixin
 
 db = SQLAlchemy()
 
+
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
-    first_name = db.Column(db.String(100))
-    last_name = db.Column(db.String(100))
+    first_name = db.Column(db.String(100), nullable=False)
+    last_name = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(255), nullable=False)
-    news = db.relationship('News', backref='author', lazy=True)
+
+    news = db.relationship(
+        'News',
+        backref='author',
+        lazy=True,
+        cascade='all, delete-orphan'
+    )
+
 
 class News(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(200))
-    content = db.Column(db.Text)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    title = db.Column(db.String(200), nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    user_id = db.Column(
+        db.Integer,
+        db.ForeignKey('user.id'),
+        nullable=False
+    )
